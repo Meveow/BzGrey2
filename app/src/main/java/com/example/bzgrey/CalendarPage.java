a46d0da06e4a9b29d26663f3ae8a51594dda0890
Meveow <karwai.soong@jcu.edu.au>
2023-05-21 20:05:20 +0800

    Complete CalendarPage

--- app/src/main/java/com/example/bzgrey/CalendarPage.java
+++ app/src/main/java/com/example/bzgrey/CalendarPage.java
@@ -7,21 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class CalenderPage extends AppCompatActivity {
+public class CalendarPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender_page);
+        setContentView(R.layout.activity_calendar_page);
 
         Button homeButton = findViewById(R.id.home);
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CalenderPage.this, MainActivity.class);
-                startActivity(intent);
-            }
+        homeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(CalendarPage.this, MainActivity.class);
+            startActivity(intent);
         });
+
     }
 }
--- app/src/main/java/com/example/bzgrey/MainActivity.java
+++ app/src/main/java/com/example/bzgrey/MainActivity.java
@@ -14,19 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button patientInfoButton = findViewById(R.id.patient_info);
-        Button calenderPageButton = findViewById(R.id.calendar_button);
+        Button calendarPageButton = findViewById(R.id.calendar_button);
         Button entertainmentPageButton = findViewById(R.id.entertainment_button);
+        Button callPageButton = findViewById(R.id.call_button);
 
         patientInfoButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, PatientInfo.class);
             startActivity(intent);
         });
-        calenderPageButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, CalenderPage.class);
+        calendarPageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CalendarPage.class);
             startActivity(intent);
         });
         entertainmentPageButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, EntertainmentPage.class);
+            startActivity(intent);
+        });
+        callPageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CallPage.class);
             startActivity(intent);
         });
 
